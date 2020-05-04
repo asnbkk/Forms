c7678c50c8ff2e848793d40cc7e6392f841ab478
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-05-04 22:08:34 +0600

    hello

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AdminFirstComponent } from './components/admin-first/admin-first.component';
+import { AdminSecondComponent } from './components/admin-second/admin-second.component';
+import { AdminThirdComponent } from './components/admin-third/admin-third.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminListComponent } from './components/admin-list/admin-list.component';
+import { TestComponent } from './components/test/test.component';
+import { AdminDetailedComponent } from './components/admin-detailed/admin-detailed.component';
+import { AdminResponsesComponent } from './components/admin-responses/admin-responses.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:  HomeComponent },
+  { path: 'login', component:  LoginComponent },
+  { path: 'admin', component: AdminComponent, children: [
+    { path: '1', component: AdminFirstComponent },
+    { path: '2', component: AdminSecondComponent},
+    { path: '3', component: AdminThirdComponent},
+    { path: 'list', component: AdminListComponent },
+    { path: 'detailed/:id', component: AdminDetailedComponent },
+    { path: 'respons/:id', component: AdminResponsesComponent }
+  ] },
+  { path: 'test/:id', component: TestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
